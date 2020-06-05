84f24869bea129931ff7e7a344bc5ad81fa557a8
Miguel Angel Vazquez Pech <36494384+MiguelAngelVazquezPech@users.noreply.github.com>
2020-06-04 20:02:01 -0500

    Add files via upload

---
+++ src/Unidad_6/MNumU6.java
@@ -0,0 +1,13 @@
+package Unidad_6;
+
+
+public class MNumU6 {
+
+    public static void main(String[] args) {
+    JPanelEuler PrimerPaso = new JPanelEuler();
+        PrimerPaso.setVisible(true);
+        
+    PrimerPaso.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ src/Unidad_6/Unidad6.java
@@ -0,0 +1,17 @@
+package Unidad_6;
+
+
+public class Unidad6 {
+
+    public static void main(String[] args) {
+       
+         
+    JPanelEuler PrimerPaso = new JPanelEuler();
+        PrimerPaso.setVisible(true);
+        
+    PrimerPaso.setLocationRelativeTo(null);
+    
+    }
+    }
+    
+
